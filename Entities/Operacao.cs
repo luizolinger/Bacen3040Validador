bdf1eca60db0d68126745c0ffd07bd1475e9ba61
Luiz Otávio Olinger Momm <luizotaviom@gmail.com>
2022-02-13 10:08:49 -0300

    Criação do projeto incial para leitura de XMLs e impressão da primeira validação

---
+++ Entities/Operacao.cs
@@ -0,0 +1,25 @@
+﻿namespace Bndes3040Revisao
+{
+    class Operacao
+    {
+        public string Ipoc { get; set; }
+        public string Modalidade { get; set; }
+        public bool PossuiSaida { get; set; }
+        public Operacao()
+        {
+
+        }
+
+        public Operacao(string ipoc, string modalide)
+        {
+            Ipoc = ipoc;
+            Modalidade = modalide;
+        }
+
+        public Operacao(string ipoc, string modalidade, bool possuiSaida) : this(ipoc, modalidade)
+        {
+            PossuiSaida = possuiSaida;
+        }
+
+    }
+}
